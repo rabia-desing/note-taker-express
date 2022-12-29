4d4e7a1a27365e2b78a3c78a2bd0e1179d13c808
rabia-desing <rabia.aman25@gmail.com>
2022-12-29 06:08:14 +0500

    fully tested code on local machine and then pushed to git repository after testing and removing bugs

---
+++ README.md
@@ -0,0 +1,33 @@
+# Express.js: Note Taker
+
+## Introduction:
+
+Notes Taker application powered by Node js with clean code. You can quickly and easily create a your work notes.
+
+You can enter note title and then enter ntoe text by clicking the add note button at top right of the page
+
+
+## Developer Insights:
+
+I am thankful to my instructors at RutgersCodingBootcamp, that they teach me so well and I have crated this coding application.
+
+## Resources:
+
+-   [W3SCHOOLS.COM](https://www.w3schools.com/nodejs/)
+-   [NODEJS](https://www.w3schools.com/nodejs/)
+
+
+## How to run:
+
+
+You can run the application by opening the terminal in your MAC and type following:
+
+NPM RUN START
+
+## ScreenShots
+
+The following images show the web application's appearance and functionality:
+
+![Existing notes are listed in the left-hand column with empty fields on the right-hand side for the new note’s title and text.](./assets/11-express-homework-demo-01.png)
+
+![Note titled “Balance accounts” reads, “Balance account books by end of day Monday,” with other notes listed on the left.](./assets/11-express-homework-demo-02.png)
---
+++ server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for reviews
+app.get('/api/notes', (req, res) => {
+    
+
+fs.readFile('./db/db.json', "utf8", function(err, data){
+    if(err) throw err;
+
+    var resultArray = JSON.parse(data);
+
+    res.status(200).json(resultArray);
+});
+
+});
+
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+    };
+      // Obtain existing notes
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new note
+          parsedNotes.push(newNote);
+  
+          // Write updated notes back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+          
+        }
+      });
+    
+
+    const response = {
+      status: 'success'
+    };
+
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in adding new note');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
